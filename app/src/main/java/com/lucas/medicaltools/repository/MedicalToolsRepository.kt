ee72c92cffc875cfd55f7b80e0e98f31ab1589b3
Lucas <maleselan@discovery.co.za>
2021-08-04 20:44:33 +0200

    Updated Medical Tools cache

--- app/src/main/java/com/lucas/medicaltools/repository/MedicalToolsRepository.kt
+++ app/src/main/java/com/lucas/medicaltools/repository/MedicalToolsRepository.kt
@@ -3,38 +3,20 @@ package com.lucas.medicaltools.repository
 import com.lucas.medical_equip.repository.MedicalTool
 import com.lucas.medical_equip.service.RetrofitBuilder
 import io.reactivex.Observable
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
 import retrofit2.Response
 
 object MedicalToolsRepository {
-    var medicalToolCache: Observable<Response<List<MedicalTool>>>? = null
-    //lateinit var medicalToolCache2: Response<List<MedicalTool>>
-
+    var medicalToolCache: Response<List<MedicalTool>>? = null
 
     fun getMedicalTools(): Observable<Response<List<MedicalTool>>> {
         return if (medicalToolCache === null) {
             var medToolsService = RetrofitBuilder.apiService
             val medToolServiceResponse = medToolsService.getMedicalEquipments()
-            cacheMedicalTools2(medToolServiceResponse)
-            medToolServiceResponse
+            medToolServiceResponse.doOnNext {
+                medicalToolCache = it
+            }
         } else {
-            medicalToolCache!!
+            Observable.just(medicalToolCache)
         }
     }
-
-    private fun cacheMedicalTools(tools: Observable<Response<List<MedicalTool>>>) {
-        medicalToolCache = tools
-    }
-
-    private fun cacheMedicalTools2(tools: Observable<Response<List<MedicalTool>>>) {
-        //medicalToolCache2 = tools.blockingFirst()
-        /*tools.switchMap {
-          if (it.isSuccessful) {
-              medicalToolCache2 = it
-          }
-        }*/
-        //medicalToolCache2
-    }
-
 }
