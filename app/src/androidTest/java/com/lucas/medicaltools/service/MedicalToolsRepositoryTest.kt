9beec72269b16b69ebd5105342b3bf6de968a3c1
Lucas <maleselan@discovery.co.za>
2021-08-11 13:27:01 +0200

    MedicalTools Interactor and Repository unit tests

--- app/src/androidTest/java/com/lucas/medicaltools/service/MedicalToolsRepositoryTest.kt
+++ app/src/androidTest/java/com/lucas/medicaltools/service/MedicalToolsRepositoryTest.kt
@@ -1,5 +1,7 @@
 package com.lucas.medicaltools.service
 
+import retrofit2.Response
+
 import com.lucas.medical_equip.repository.MedicalTool
 import com.lucas.medical_equip.service.APIService
 import com.lucas.medical_equip.service.RetrofitBuilder
@@ -23,16 +25,11 @@ import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(JUnit4::class)
-class MedicalToolsInteractorTest {
+class MedicalToolsRepositoryTest  {
 
 
     @Mock
-    private lateinit var medicalToolsInteractor: MedicalToolsInteractor
-
-    @Mock
-    private lateinit var dataState: MainViewState.MedicalToolsState
-    @Mock
-    private lateinit var errorState: MainViewState.MedicalToolsState
+    private lateinit var medicalToolsRepo: MedicalToolsRepository
 
     companion object {
         @Throws(Exception::class)
@@ -54,40 +51,46 @@ class MedicalToolsInteractorTest {
     @Before
     fun setUp() {
         init()
-        //MockitoAnnotations.initMocks(this)
-        medicalToolsInteractor = MedicalToolsInteractor
+        medicalToolsRepo = MedicalToolsRepository
+    }
+
+    @Test
+    fun `medicalToolsListIsNotEmpty`() {
+        val results = medicalToolsRepo.getMedicalTools()
+            .blockingFirst()
+        Assert.assertNotNull(results)
+    }
+    lateinit var results: Response<List<MedicalTool>>
+
+    @Test
+    fun medicalToolsListIsGreaterThanOne() {
+        val results = medicalToolsRepo.getMedicalTools()
+            .blockingFirst()
+        Assert.assertTrue(results.body()?.size!! > 1)
+    }
+
+    @Test
+    fun medicalToolsHaveDescription() {
+        val results = medicalToolsRepo.getMedicalTools()
+            .blockingFirst()
+        // Assert.assertTrue(results.body()?.size!! > 1)
+        for (medicalTool in results.body()!!) {
+            Assert.assertNotNull(medicalTool.description)
+        }
+    }
+
+    @Test
+    fun medicalToolsHaveImage() {
+        val results = medicalToolsRepo.getMedicalTools()
+            .blockingFirst()
+        // Assert.assertTrue(results.body()?.size!! > 1)
+        for (medicalTool in results.body()!!) {
+            Assert.assertNotNull(medicalTool.imageURL)
+        }
     }
 
     @After
     fun tearDown() {
         //tearDown()
-
-    }
-
-    @Test
-    fun testGetMedicalToolsSuccess() {
-        val filterString = ""
-        val viewState = medicalToolsInteractor.getMedicalTools(filterString)
-            .blockingFirst()
-        Assert.assertTrue(!viewState.equals(MainViewState.LoadingState))
-        Assert.assertTrue(!viewState.equals(MainViewState.ErrorState))
-    }
-
-    @Test
-    fun `medicalToolsListIsNotEmpty`() {
-        var filter: String = ""
-        Assert.assertNotNull(filter)
-    }
-
-    @Test
-    fun getMedicalToolsFailure() {
-    }
-
-    @Test
-    fun setMedicalToolsFilterMap() {
-    }
-
-    @Test
-    fun getMedicalTools() {
     }
 }
